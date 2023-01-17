eab5c1a6909babbc5dabde5de354c93ff75c40d6
LAPTOP-EKABBP8Q\Coding Ninjas <content@codingninjas.com>
2023-01-17 14:03:30 +0530

    // Update project for Movie booking REST API

--- MovieBookingAPI/src/main/java/com/example/MovieTicket/MovieBooking/service/MovieService.java
+++ MovieBookingAPI/src/main/java/com/example/MovieTicket/MovieBooking/service/MovieService.java
@@ -5,34 +5,19 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
 
 import com.example.MovieTicket.MovieBooking.Exceptions.IdNotFound;
 import com.example.MovieTicket.MovieBooking.Model.Movie;
-import com.example.MovieTicket.MovieBooking.communicator.RatingRestCommunicator;
 
 @Service
 public class MovieService implements MovieServiceInterface {
 	
 	List<Movie> moviesList =new ArrayList<>();
 	Map<String,Movie> movieMap= new HashMap<>();
-	
-	@Autowired
-	RatingRestCommunicator ratingServiceCommunicator;
-	
-	
-//	public MovieService() {
-////		super();
-////	moviesList=new ArrayList<>();
-////		moviesList.add(new Movie(1,"Captain America","Joe Johnston ", "6.9", "English",
-////				new ArrayList<>(List.of("Christopher Markus", "Stephen McFeely","Joe Simon")),
-////				new ArrayList<>(List.of("Chris Evans", "Stephen McFeely","Samuel L. Jackson")),
-////				new ArrayList<>(List.of("Adventures", "Action"))
-////			));
-//	}
+
 
 	@Override
 	public List<Movie> getMovies() {
@@ -47,20 +32,14 @@ public class MovieService implements MovieServiceInterface {
 			throw new IdNotFound("Hotel not found for id: "+id);
 		}
 		Movie movie = movieMap.get(id);
-		
-		long updatedRating=ratingServiceCommunicator.getRating(id);
-		movie.setMovieRating(updatedRating);
 		return movie;
 	}
 
 	@Override
 	public void addMovie(Movie movie) {
-		Map<String, Long> ratingsMap = new HashMap<>();
 		
 		moviesList.add(movie); 
 		movieMap.put(movie.getId(), movie);
-		ratingsMap.put(movie.getId(), movie.getMovieRating());
-		ratingServiceCommunicator.addRating(ratingsMap);
 	}
 		
 
@@ -69,7 +48,6 @@ public class MovieService implements MovieServiceInterface {
 		Movie movie = getMovie(id);
 		moviesList.remove(movie);
 		movieMap.remove(id);
-		ratingServiceCommunicator.deleteRating(id);
 	}
 
 	@Override
@@ -79,10 +57,6 @@ public class MovieService implements MovieServiceInterface {
 		moviesList.remove(existingMovie);
 		moviesList.add(movie);		
 		movieMap.put(id, movie);
-		
-		Map<String,Long> updatedRating = new HashMap<>();
-		updatedRating.put(id, movie.getMovieRating());
-		ratingServiceCommunicator.updateRating(updatedRating);
 	}
 
 }
